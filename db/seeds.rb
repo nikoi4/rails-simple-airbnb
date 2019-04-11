f511682749b8b6fe9f1ea5826b8f972a60d954d0
Nicolas Caif <r.nicolascaif@gmail.com>
2019-04-11 19:30:46 -0300

    added flat model and controller with index new and create method and its views as well

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Seeding'
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Andes Mountains sights flat',
+  address: 'Emilio Civit 29, Mendoza, Argentina',
+  description: 'Spectacular sight to Andes mountain chain. A studio fully equipped with king size bed, refrigeration and a jacuzzi',
+  price_per_night: 95,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: '360 view from PH in Palermo Norte',
+  address: 'Thames 2300, Palermo, Buenos Aires',
+  description: 'Best panoramic view of Buenos Aires and if like weather of Uruguay Coasts. 4 Bedrooms in suite, spacious kitchen and dining room',
+  price_per_night: 145,
+  number_of_guests: 6
+)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 65,
+  number_of_guests: 3
+)
+
+puts 'Seeding successful'
